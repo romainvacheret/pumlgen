1e55c72076a67a02b213fc69be1fcd80a7f76c87
Romain Vacheret <romain.vacheret@gmail.com>
2021-10-03 17:45:35 +0200

    feat: add cli

--- app/src/main/java/pumlgen/App.java
+++ app/src/main/java/pumlgen/App.java
@@ -2,15 +2,37 @@ package pumlgen;
 
 import java.io.File;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
 import pumlgen.files.impl.FileWalkerImpl;
 import pumlgen.files.models.FileWalker;
 import pumlgen.uml.builders.UMLAbstractBuilder;
 
 public class App {
 
-    public static void main(String[] args) {
+    @Parameter(names = {"--path", "-p"})
+    private String path;
+
+    @Parameter(names = {"--example", "-e"})
+    private boolean isExampleSeleted = false;
+
+    private static final String EXAMPLE = "src/main/resources";
+    public static void main(String... argv) {
+        final App app = new App();
+
+        JCommander.newBuilder()
+            .addObject(app)
+            .build()
+            .parse(argv);
+        app.run();
+    }
+
+    public void run() {
         final FileWalker walker = new FileWalkerImpl();
-        walker.walk(new File("app/src/main/resources"))
+        final String finalPath = isExampleSeleted ? EXAMPLE : path;
+
+        walker.walk(new File(finalPath))
             .map(UMLAbstractBuilder::build)
             .ifPresent(System.out::println);
     }
